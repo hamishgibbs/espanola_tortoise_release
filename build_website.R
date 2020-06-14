b65924e307d1abbf58ded9cc353b07a8a05208be
hamishgibbs <hamishgibbs@gmail.com>
2020-06-14 07:52:20 +0100

    automatic site build

--- build_website.R
+++ build_website.R
@@ -2,9 +2,3 @@ source('test_website_build.R')
 Sys.setenv(RSTUDIO_PANDOC = "/Applications/RStudio.app/Contents/MacOS/pandoc")
 rmarkdown::render_site()
 test_website_build()
-
-r <- readline(prompt="Push new website (y/n)?")
-
-if(r == 'n'){
-  stop("Website push aborted.")
-}
